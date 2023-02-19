642e8707a518686e7afb8bc36b2d6540b9977fd0
Admin <withyouamalive@yahoo.com>
2023-02-19 08:52:00 +0100

    another few changes

--- App.tsx
+++ App.tsx
@@ -15,6 +15,7 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   useColorScheme,
   View,
 } from 'react-native';
@@ -35,6 +36,7 @@ const Section: React.FC<
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
+      <TextInput value='My Name'/>
       <Text
         style={[
           styles.sectionTitle,
